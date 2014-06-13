972b6ab74c768f83d07804c4ecc0de5371228c54
strahjuanpablo@gmail.com <strahjuanpablo@gmail.com@5deedd4e-507c-d0d8-508a-02bb76c10c73>
2014-06-13 21:58:20 +0000

    1commit del tp

---
+++ trunk/TPFinalObjetos2/src/SistemaGestionProductos/Presentacion.java
@@ -0,0 +1,18 @@
+package SistemaGestionProductos;
+
+import java.util.ArrayList;
+
+public class Presentacion {
+	
+	String codDebarras;
+	int stockMinimo;
+	int stockCritico;
+	double precioVenta;
+	double precioCompra;
+	Producto producto;
+	ArrayList<Double>precioHistorico = new ArrayList<Double>();
+	
+
+	
+	public Presentacion 
+}
