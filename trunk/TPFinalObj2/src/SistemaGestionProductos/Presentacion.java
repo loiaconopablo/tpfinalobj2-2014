fca57c9aec2e0a549cf7767835224b133e115cf8
loiaconopablo <loiaconopablo@5deedd4e-507c-d0d8-508a-02bb76c10c73>
2014-06-25 02:17:55 +0000

    Seguir con este trunk

---
+++ trunk/TPFinalObj2/src/SistemaGestionProductos/Presentacion.java
@@ -0,0 +1,22 @@
+package SistemaGestionProductos;
+
+import java.util.ArrayList;
+
+public class Presentacion {
+	
+	private String codDebarras;
+	private int stockMinimo;
+	private int stockCritico;
+	private double precioVenta;
+	private double precioCompra;
+	private Producto producto;
+	private ArrayList<Double>precioHistorico = new ArrayList<Double>();
+	
+	public double getPrecioVenta() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	
+}
---
+++ trunk/TPFinalObj2/src/SistemaGestionProductos/Producto.java
@@ -0,0 +1,91 @@
+package SistemaGestionProductos;
+
+import java.util.ArrayList;
+
+
+public class Producto {
+	
+	private String descripcion;
+	private String nombre;
+	private ArrayList<Presentacion> presentaciones = new ArrayList<Presentacion>();
+	private String tipo;
+	private String unidadMedida;
+	private String marca;
+
+	
+	public Producto (String descrip, String nom, ArrayList<Presentacion> presen, String tipo, String unidadMedida){
+		
+		this.descripcion = descrip; 
+		this.nombre = nom;
+		this.presentaciones = presen;
+		this.tipo = tipo;
+		this.unidadMedida = unidadMedida;
+	}
+	
+	
+//////////////////////////getters&setters////////////////////////////////////
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public ArrayList<Presentacion> getPresentaciones() {
+		return presentaciones;
+	}
+
+	private void setPresentaciones(ArrayList<Presentacion> presentaciones) {
+		this.presentaciones = presentaciones;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	private void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getUnidadMedida() {
+		return unidadMedida;
+	}
+
+	private void setUnidadMedida(String unidadMedida) {
+		this.unidadMedida = unidadMedida;
+	}
+
+	
+///////////////////////////////////////////////////////////////////////////////////	
+	
+	public boolean tengoPresentacion(Presentacion p){
+		
+		return this.presentaciones.contains(p);
+	}
+	
+	public boolean esDeMiTipo(String t){
+		
+		return getTipo()==t;
+	}
+	
+	public void agregarPresentacion(Presentacion p){
+		
+		this.presentaciones.add(p);
+	}
+	
+	public void quitarPresentacion(Presentacion p){
+		
+		this.presentaciones.remove(p);
+	}
+}
+
+
