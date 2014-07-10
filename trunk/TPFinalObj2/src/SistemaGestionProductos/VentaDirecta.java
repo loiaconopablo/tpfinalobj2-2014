470a765d161bc0bc3f9f8b9d56a6e029fb7201c5
loiaconopablo <loiaconopablo@5deedd4e-507c-d0d8-508a-02bb76c10c73>
2014-07-10 12:27:46 +0000

    Algo de ventas. Hoy me pongo en el laburo y cuando llego a casa desde las 18:30.

--- trunk/TPFinalObj2/src/SistemaGestionProductos/VentaDirecta.java
+++ trunk/TPFinalObj2/src/SistemaGestionProductos/VentaDirecta.java
@@ -2,14 +2,24 @@ package SistemaGestionProductos;
 
 public class VentaDirecta extends Venta {
 
-	public VentaDirecta(Pedido p, Cliente c) {
-		this.pedido = p;
-		this.cliente = c;
-	}
-
 	public VentaDirecta(Pedido p) {
-		this.pedido = p;
+		this.setPedido(p);
+		this.setCliente(new Cliente("Generico",null,null));
 	}
 
+	public VentaDirecta(Cliente c, Pedido p) {
+		this.setPedido(p);
+		this.setCliente(c);
+	}
+
+	public Venta generarVenta(Pedido p) {
+		VentaDirecta nueva= new VentaDirecta(p);
+		return nueva;
+	}
 	
+	@Override
+	public VentaDirecta generarVenta(Cliente c, Pedido p) {
+		VentaDirecta nueva= new VentaDirecta(c,p);
+		return nueva;
+	}
 }
