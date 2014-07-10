6843566e05e5ddc50add634327713448eebf62e5
loiaconopablo <loiaconopablo@5deedd4e-507c-d0d8-508a-02bb76c10c73>
2014-07-10 23:56:36 +0000

    Voy subiendo cambios, modifique varios metodos en sucursal que estaban mal mas que nada los contructores de sucu y negocio y fui agregando cosas, como agregar sucursales nuevas, nuevos clientes, tener un cliente generico, tengo que seguir con el tema ventas y los reportes. Voy comiteando a medida que avanzo.. hoy trato de terminar todo lo mio xq mñana en el laburo casi imposible que pueda hacer algo.

--- TPFinalObj2/src/SistemaGestionProductos/Sucursal.java
+++ TPFinalObj2/src/SistemaGestionProductos/Sucursal.java
@@ -6,21 +6,77 @@ import java.util.List;
 public class Sucursal {
 
 	private Stock stock;
-	int idSucursal;
+	private int idSucursal;
 	private List<Ubicacion>ubicaciones = new ArrayList<Ubicacion>();
-	private List<Oferta>ofertas = new ArrayList<Oferta>();
-	private List<Venta> historialDeVentas = new ArrayList<Venta>();
+	private List<Oferta>ofertas;
+	private List<Venta> historialDeVentas;
 	private Negocio negocio;
 	
-	public Sucursal(Stock stock, int id, List<Ubicacion>ubicaciones, List<Oferta>ofertas,List<Venta>ventas){
+	public Sucursal(int id, List<Ubicacion>ubicaciones, Negocio negocio){
 		
-		this.stock = stock;
-		this.idSucursal = idSucursal;
+		this.setNegocio(negocio);
+		this.stock = new Stock();
+		this.idSucursal = id;
 		this.ubicaciones = ubicaciones;
-		this.ofertas = ofertas;
-		this.historialDeVentas = ventas;
+		this.setOfertas(new ArrayList<Oferta>());
+		this.setHistorialDeVentas(new ArrayList<Venta>());
+	}
+
+	public void agregarMercaderiaALaSucursal(Pedido pedido){
+		this.getStock().agregarPedidoAlStock(pedido);
+	}
+	public void nuevoCliente(String nombreCl,  int dni, String direccion){
+		this.getNegocio().nuevoCliente(nombreCl, dni, direccion);
+	}
+
+	public void efectuarVentaComun(Pedido pedido, Cliente cliente) throws NoPuedeDescontarException{
+		VentaDirecta venta = new VentaDirecta(cliente,pedido);
+		this.getHistorialDeVentas().add(venta);
+		//SE SUPONE QUE SIEMPRE HAY STOCK
+		this.getStock().descontarPedidoDelStock(pedido);
+		cliente.getHistorialCompras().add(venta);
 	}
 	
+	public void efectuarVentaComun(Pedido pedido) throws NoPuedeDescontarException{
+		VentaDirecta venta = new VentaDirecta(this.getNegocio().getUserGenerico(),pedido);
+		this.getHistorialDeVentas().add(venta);
+		//SE SUPONE QUE SIEMPRE HAY STOCK
+		this.getStock().descontarPedidoDelStock(pedido);
+		this.getNegocio().getUserGenerico().getHistorialCompras().add(venta);
+	}
+	
+	
+	public void efectuarVentaCC(Pedido pedido, Cliente c){
+		VentaCC venta = new VentaCC(pedido, c);
+		this.getHistorialDeVentas().add(venta);
+		c.getHistorialCompras().add(venta);
+		//falta hacer metodo para agregar el pedido a la cuenta corriente, y ademas hacer el descuento del monto a la cuenta
+		
+	}
+	
+	public void efectuarVentaADomicilio(Pedido p, Cliente c){
+		
+	}
+	
+	public void reponerStock(Pedido pedido){
+		this.getStock().agregarPedidoAlStock(pedido);
+	}
+	
+	public void agregarPresentaciones(Presentacion presen, int cant, Ubicacion u ){
+		//hay que cambiar el nombre de este metodo, ya que agrega fichas al stock, no presentaciones como dice el UML.
+		Ficha ficha = new Ficha(presen, cant);
+		this.getStock().agregarFichaAlStock(ficha);
+	}
+	
+	//GETTERS Y SETTERS
+	public Negocio getNegocio() {
+		return negocio;
+	}
+
+	public void setNegocio(Negocio negocio) {
+		this.negocio = negocio;
+	}
+
 	public Stock getStock() {
 		return stock;
 	}
@@ -61,36 +117,4 @@ public class Sucursal {
 		this.historialDeVentas = historialDeVentas;
 	}
 	
-	public void efectuarVentaComun(Pedido p, Cliente c){
-		VentaDirecta venta = new VentaDirecta(p,c);
-		this.getHistorialDeVentas().add(venta);
-		c.getHistorialCompras().add(venta);
-	}
-	
-	public void efectuarVentaComun(Pedido p){
-		VentaDirecta venta = new VentaDirecta(p);
-		this.getHistorialDeVentas().add(venta);
-	}
-	
-	public void efectuarVentaCC(Pedido pedido, Cliente c){
-		VentaCC venta = new VentaCC(pedido, c);
-		this.getHistorialDeVentas().add(venta);
-		c.getHistorialCompras().add(venta);
-		//falta hacer metodo para agregar el pedido a la cuenta corriente, y ademas hacer el descuento del monto a la cuenta
-		
-	}
-	
-	public void efectuarVentaADomicilio(Pedido p, Cliente c){
-		
-	}
-	
-	public void reponerStock(Pedido pedido){
-		this.getStock().agregarPedidoAlStock(pedido);
-	}
-	
-	public void agregarPresentaciones(Presentacion presen, int cant, Ubicacion u ){
-		//hay que cambiar el nombre de este metodo, ya que agrega fichas al stock, no presentaciones como dice el UML.
-		Ficha ficha = new Ficha(presen, cant);
-		this.getStock().agregarFichaAlStock(ficha);
-	}
 }
--- TPFinalObj2/src/SistemaGestionProductos/VentaCC.java
+++ TPFinalObj2/src/SistemaGestionProductos/VentaCC.java
@@ -3,9 +3,7 @@ package SistemaGestionProductos;
 public class VentaCC extends Venta {
 
 	public VentaCC (Pedido p, Cliente c){
-		
-		this.pedido = p;
-		this.cliente = c;
+	
 	}
 
 }
