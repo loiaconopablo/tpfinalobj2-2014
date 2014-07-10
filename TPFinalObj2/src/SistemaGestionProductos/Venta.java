62a2d8658e0bcb81910617bbcbdf0ef6af65d131
loiaconopablo <loiaconopablo@5deedd4e-507c-d0d8-508a-02bb76c10c73>
2014-07-10 12:27:46 +0000

    Algo de ventas. Hoy me pongo en el laburo y cuando llego a casa desde las 18:30.

--- TPFinalObj2/src/SistemaGestionProductos/Venta.java
+++ TPFinalObj2/src/SistemaGestionProductos/Venta.java
@@ -4,28 +4,34 @@ import java.util.Date;
 
 public abstract class Venta {
 	
-	protected Cliente cliente;
-	protected Date fechaDeVenta;
-	protected Pedido pedido;
+	private Cliente cliente;
+	private Date fechaDeVenta;
+	private Pedido pedido;
 	
 	public Cliente getCliente() {
 		return cliente;
 	}
+
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
+
 	public Date getFechaDeVenta() {
 		return fechaDeVenta;
 	}
+
 	public void setFechaDeVenta(Date fechaDeVenta) {
 		this.fechaDeVenta = fechaDeVenta;
 	}
+
 	public Pedido getPedido() {
 		return pedido;
 	}
+
 	public void setPedido(Pedido pedido) {
 		this.pedido = pedido;
 	}
-	
-	
+
+	public abstract Venta generarVenta(Cliente c, Pedido p);
+
 }
