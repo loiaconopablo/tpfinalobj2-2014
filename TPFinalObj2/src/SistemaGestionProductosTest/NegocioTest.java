a9537a29182bba1102e4f80b026beb4e6a522b77
loiaconopablo@gmail.com <loiaconopablo@gmail.com@5deedd4e-507c-d0d8-508a-02bb76c10c73>
2014-07-11 21:24:03 +0000

    ultimos cambios

---
+++ TPFinalObj2/src/SistemaGestionProductosTest/NegocioTest.java
@@ -0,0 +1,112 @@
+/**
+ * 
+ */
+package SistemaGestionProductosTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Loiacono.P671
+ *
+ */
+public class NegocioTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link SistemaGestionProductos.Negocio#Negocio(java.lang.String)}.
+	 */
+	@Test
+	public void testNegocio() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SistemaGestionProductos.Negocio#devolverSucural(int)}.
+	 */
+	@Test
+	public void testDevolverSucural() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SistemaGestionProductos.Negocio#generarNuevaSucursal(int)}.
+	 */
+	@Test
+	public void testGenerarNuevaSucursal() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SistemaGestionProductos.Negocio#generarListadoDeVenta()}.
+	 */
+	@Test
+	public void testGenerarListadoDeVenta() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SistemaGestionProductos.Negocio#generarListadoVenta(java.util.Date, java.util.Date)}.
+	 */
+	@Test
+	public void testGenerarListadoVentaDateDate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SistemaGestionProductos.Negocio#generarListadoVenta(java.util.Date)}.
+	 */
+	@Test
+	public void testGenerarListadoVentaDate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SistemaGestionProductos.Negocio#generarListadoVenta(double, double)}.
+	 */
+	@Test
+	public void testGenerarListadoVentaDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SistemaGestionProductos.Negocio#montoCompradoPor(SistemaGestionProductos.Cliente)}.
+	 */
+	@Test
+	public void testMontoCompradoPorCliente() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SistemaGestionProductos.Negocio#montoCompradoPor(SistemaGestionProductos.Cliente, java.util.Date)}.
+	 */
+	@Test
+	public void testMontoCompradoPorClienteDate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SistemaGestionProductos.Negocio#getUserGenerico()}.
+	 */
+	@Test
+	public void testGetUserGenerico() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link SistemaGestionProductos.Negocio#nuevoCliente(java.lang.String, int, java.lang.String)}.
+	 */
+	@Test
+	public void testNuevoCliente() {
+		fail("Not yet implemented");
+	}
+
+}
