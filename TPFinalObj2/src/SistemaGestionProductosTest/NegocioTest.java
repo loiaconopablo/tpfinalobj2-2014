9c21ecd1d13f7cc4131f6d91c717cc16cd2fd787
loiaconopablo@gmail.com <loiaconopablo@gmail.com@5deedd4e-507c-d0d8-508a-02bb76c10c73>
2014-07-18 21:45:23 +0000

    Ultimo Commit.

--- TPFinalObj2/src/SistemaGestionProductosTest/NegocioTest.java
+++ TPFinalObj2/src/SistemaGestionProductosTest/NegocioTest.java
@@ -1,112 +1,55 @@
-/**
- * 
- */
 package SistemaGestionProductosTest;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.*;
 
-/**
- * @author Loiacono.P671
- *
- */
+import SistemaGestionProductos.Cliente;
+import SistemaGestionProductos.Negocio;
+import SistemaGestionProductos.Sucursal;
+
 public class NegocioTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
+	Negocio negocio;
+	Cliente cliente;
+	Sucursal sucur;
 	@Before
 	public void setUp() throws Exception {
+		negocio = new Negocio("COTO");
+		cliente = Mockito.mock(Cliente.class);
+		sucur = Mockito.mock(Sucursal.class);
+		when(sucur.getIdSucursal()).thenReturn(5);
+		
+		
 	}
 
-	/**
-	 * Test method for {@link SistemaGestionProductos.Negocio#Negocio(java.lang.String)}.
-	 */
 	@Test
 	public void testNegocio() {
-		fail("Not yet implemented");
+		assertEquals("COTO", negocio.getNombre());
+		assertTrue(negocio.getClientes().size()==1);
+		assertTrue(negocio.getSucursales().size()==0);
+		assertTrue(negocio.getCaja()==0.00);		
 	}
-
-	/**
-	 * Test method for {@link SistemaGestionProductos.Negocio#devolverSucural(int)}.
-	 */
+	
 	@Test
-	public void testDevolverSucural() {
-		fail("Not yet implemented");
+	public void TestGenerarNuevaSucursal(){
+		negocio.generarNuevaSucursal(1);
+		assertTrue(negocio.getSucursales().size()==1);
+		
 	}
-
-	/**
-	 * Test method for {@link SistemaGestionProductos.Negocio#generarNuevaSucursal(int)}.
-	 */
 	@Test
-	public void testGenerarNuevaSucursal() {
-		fail("Not yet implemented");
+	public void TestAgregarCliente(){
+	negocio.agregarCliente(cliente);
+		assertTrue(negocio.getClientes().size()==2);
+		assertEquals(cliente, negocio.getClientes().get(1));
 	}
-
-	/**
-	 * Test method for {@link SistemaGestionProductos.Negocio#generarListadoDeVenta()}.
-	 */
 	@Test
-	public void testGenerarListadoDeVenta() {
-		fail("Not yet implemented");
+	public void TestDevolverSucural(){
+		negocio.getSucursales().add(sucur);
+		assertEquals(sucur,negocio.devolverSucural(5));
 	}
-
-	/**
-	 * Test method for {@link SistemaGestionProductos.Negocio#generarListadoVenta(java.util.Date, java.util.Date)}.
-	 */
-	@Test
-	public void testGenerarListadoVentaDateDate() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link SistemaGestionProductos.Negocio#generarListadoVenta(java.util.Date)}.
-	 */
-	@Test
-	public void testGenerarListadoVentaDate() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link SistemaGestionProductos.Negocio#generarListadoVenta(double, double)}.
-	 */
-	@Test
-	public void testGenerarListadoVentaDoubleDouble() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link SistemaGestionProductos.Negocio#montoCompradoPor(SistemaGestionProductos.Cliente)}.
-	 */
-	@Test
-	public void testMontoCompradoPorCliente() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link SistemaGestionProductos.Negocio#montoCompradoPor(SistemaGestionProductos.Cliente, java.util.Date)}.
-	 */
-	@Test
-	public void testMontoCompradoPorClienteDate() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link SistemaGestionProductos.Negocio#getUserGenerico()}.
-	 */
-	@Test
-	public void testGetUserGenerico() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link SistemaGestionProductos.Negocio#nuevoCliente(java.lang.String, int, java.lang.String)}.
-	 */
-	@Test
-	public void testNuevoCliente() {
-		fail("Not yet implemented");
-	}
-
+	
 }
